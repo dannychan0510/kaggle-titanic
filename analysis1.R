c3c90326de16828e789c179fcb3602e7fe7b3ba3
Danny Chan <dannychan0510@gmail.com>
2015-04-15 18:49:50 +0100

    update

--- analysis1.R
+++ analysis1.R
@@ -104,3 +104,7 @@ fancyRpartPlot(fit)
 Prediction <- predict(fit, test, type = "class")
 submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
 write.csv(submit, file = "submission4.csv", row.names = FALSE)
+
+
+
+# This is a different comment (B)
